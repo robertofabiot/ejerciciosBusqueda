2ac8306b6bffb8965586407c064e0de830a65f90
robertofabiot <rftercerom@uamv.edu.ni>
2025-11-20 23:30:10 -0600

    Add ejercicio4, binary search pending

---
+++ Ejercicio1/Ejercicios/Ejercicio4.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Ejercicio1.Ejercicios
+{
+    partial class Ejercicio4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEstudiantes = new System.Windows.Forms.DataGridView();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.btnBuscarIDLineal = new System.Windows.Forms.Button();
+            this.btnBuscarNombreBinaria = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstudiantes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEstudiantes
+            // 
+            this.dgvEstudiantes.AllowUserToOrderColumns = true;
+            this.dgvEstudiantes.AllowUserToResizeColumns = false;
+            this.dgvEstudiantes.AllowUserToResizeRows = false;
+            this.dgvEstudiantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEstudiantes.Location = new System.Drawing.Point(115, 211);
+            this.dgvEstudiantes.Name = "dgvEstudiantes";
+            this.dgvEstudiantes.RowHeadersWidth = 51;
+            this.dgvEstudiantes.RowTemplate.Height = 24;
+            this.dgvEstudiantes.Size = new System.Drawing.Size(342, 331);
+            this.dgvEstudiantes.TabIndex = 0;
+            // 
+            // tbID
+            // 
+            this.tbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbID.Location = new System.Drawing.Point(100, 37);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(189, 28);
+            this.tbID.TabIndex = 1;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNombre.Location = new System.Drawing.Point(100, 117);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(189, 28);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // btnBuscarIDLineal
+            // 
+            this.btnBuscarIDLineal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarIDLineal.Location = new System.Drawing.Point(304, 20);
+            this.btnBuscarIDLineal.Name = "btnBuscarIDLineal";
+            this.btnBuscarIDLineal.Size = new System.Drawing.Size(166, 62);
+            this.btnBuscarIDLineal.TabIndex = 3;
+            this.btnBuscarIDLineal.Text = "Buscar ID (lineal)";
+            this.btnBuscarIDLineal.UseVisualStyleBackColor = true;
+            this.btnBuscarIDLineal.Click += new System.EventHandler(this.btnBuscarIDLineal_Click);
+            // 
+            // btnBuscarNombreBinaria
+            // 
+            this.btnBuscarNombreBinaria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarNombreBinaria.Location = new System.Drawing.Point(304, 101);
+            this.btnBuscarNombreBinaria.Name = "btnBuscarNombreBinaria";
+            this.btnBuscarNombreBinaria.Size = new System.Drawing.Size(166, 61);
+            this.btnBuscarNombreBinaria.TabIndex = 4;
+            this.btnBuscarNombreBinaria.Text = "Buscar nombre (binaria)";
+            this.btnBuscarNombreBinaria.UseVisualStyleBackColor = true;
+            this.btnBuscarNombreBinaria.Click += new System.EventHandler(this.btnBuscarNombreBinaria_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 177);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(573, 23);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "lblResultado";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResultado.Visible = false;
+            // 
+            // Ejercicio4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(597, 569);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnBuscarNombreBinaria);
+            this.Controls.Add(this.btnBuscarIDLineal);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.dgvEstudiantes);
+            this.Name = "Ejercicio4";
+            this.Text = "Ejercicio4";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstudiantes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEstudiantes;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button btnBuscarIDLineal;
+        private System.Windows.Forms.Button btnBuscarNombreBinaria;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ Ejercicio1/Ejercicios/Ejercicio4.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1.Ejercicios
+{
+    public partial class Ejercicio4 : Form
+    {
+        class Estudiante
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        //Colores
+        private Color colorRecorrido = Color.Blue;
+        private Color colorCorrecto = Color.Green;
+        private Color colorDefecto;
+
+        private List<Estudiante> estudiantes = new List<Estudiante>()
+        {
+            new Estudiante { Id = 1, Nombre = "Ana López" },
+            new Estudiante { Id = 2, Nombre = "Carlos Martínez" },
+            new Estudiante { Id = 3, Nombre = "Sofía Herrera" },
+            new Estudiante { Id = 4, Nombre = "Luis González" },
+            new Estudiante { Id = 5, Nombre = "María Fernández" },
+            new Estudiante { Id = 6, Nombre = "Diego Sánchez" },
+            new Estudiante { Id = 7, Nombre = "Elena Torres" },
+            new Estudiante { Id = 8, Nombre = "Javier Ruiz" },
+            new Estudiante { Id = 9, Nombre = "Gabriela Castro" },
+            new Estudiante { Id = 10, Nombre = "Fernando Vega" }
+        };
+
+        public Ejercicio4()
+        {
+            InitializeComponent();
+            dgvEstudiantes.DataSource = new List<Estudiante>(estudiantes);
+            dgvEstudiantes.DefaultCellStyle.SelectionBackColor = dgvEstudiantes.DefaultCellStyle.BackColor;
+            dgvEstudiantes.DefaultCellStyle.SelectionForeColor = dgvEstudiantes.DefaultCellStyle.ForeColor;
+        }
+
+        private async void btnBuscarIDLineal_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            string indexIDEncontrado = null;
+            string idBuscado = tbID.Text.ToString();
+            lblResultado.Text = "Haciendo búsqueda lineal...";
+            lblResultado.Visible = true;
+            foreach (DataGridViewRow filaActual in dgvEstudiantes.Rows)
+            {
+                i = 0;
+                foreach (DataGridViewCell celdaActual in filaActual.Cells)
+                {
+                    if (i == 0)
+                    {
+                        celdaActual.Style.ForeColor = colorRecorrido;
+                        await Task.Delay(300);
+                        if (celdaActual.Value.ToString() == idBuscado)
+                        {
+                            celdaActual.Style.ForeColor = colorCorrecto;
+                            celdaActual.Style.SelectionForeColor = colorCorrecto;
+                            indexIDEncontrado = celdaActual.RowIndex.ToString();
+                        }
+                        else
+                        {
+                            celdaActual.Style.ForeColor = colorDefecto;
+                        }
+                    }
+                    i = 1;
+                }
+            }
+            if(indexIDEncontrado != null)
+            {
+                lblResultado.Text = $"Encontrado en el índice: {indexIDEncontrado}";
+            }
+        }
+
+        private void OrdenarFilas()
+        {
+            dgvEstudiantes.Sort(dgvEstudiantes.Columns["Nombre"], ListSortDirection.Ascending);
+
+        }
+
+        private void btnBuscarNombreBinaria_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewColumn column in dgvEstudiantes.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.Automatic;
+            }
+        }
+    }
+}
